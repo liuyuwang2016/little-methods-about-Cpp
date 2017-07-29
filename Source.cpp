9274bb179c5fcc5ccfb732d73075e22a0ccb7ccc
Yvan Liu <lywnku@126.com>
2017-07-29 23:24:55 +0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,778 @@
+﻿/*--------------Standard Library-------------*/
+#include <iostream>
+#include <math.h>
+
+/*--------------OpenCV-------------*/
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <cv.h>
+
+/*--------------Kinect-------------*/
+#include <Kinect.h>
+#include <windows.h>   
+
+/*--------------OpenGL-------------*/
+#include <gl/Gl.h>
+#include <gl/glu.h>
+#include <gl/glut.h>
+
+using namespace std;
+using namespace cv;
+
+/*----------------------------------------------*/
+/*----------------OpenGL Declare----------------*/
+/*----------------------------------------------*/
+GLuint textureid;                      //BG texture
+int FPS = 0;
+BYTE* pBufferColor_RGB = nullptr;
+GLfloat fAspect;
+bool IsDrawPC = false;
+
+void GLInit(void);                         //OpenGL Initial
+void SpecialKeys(int key, int x, int y);   //OpenGL Key Func
+void timer(int value);                     //OpenGL Time Func
+void RenderScene(void);                    //OpenGL Render Func
+void SceneWithBackground(void);
+void DrawPointCloud(void);
+
+
+/*----------------------------------------------*/
+/*----------------Kinect Declare----------------*/
+/*----------------------------------------------*/
+//Initial (Sensor)
+IKinectSensor* pSensor = nullptr;
+ICoordinateMapper* Mapper = nullptr;
+IDepthFrameReader* pFrameReaderDepth = nullptr;
+IColorFrameReader* pFrameReaderColor = nullptr;
+void KineticInit(void);           //Kinect Initial
+void KineticUpdate(void);         //Kinect Update Frame
+
+//Depth Map
+int iWidthDepth = 0;
+int iHeightDepth = 0;
+UINT depthPointCount = 0;
+UINT16* pBufferDepth = nullptr;   //Depth map origin format
+UINT16 uDepthMin = 0, uDepthMax = 0;
+Mat mDepthImg;                    //Depth map UINT16
+Mat mImg8bit;                     //Depth map CV_8U
+
+//Color Map
+int iWidthColor = 0;
+int iHeightColor = 0;
+UINT colorPointCount = 0;
+UINT uBufferSizeColor = 0;
+BYTE* pBufferColor = nullptr;    //Color map origin format (RGBA)
+Mat mColorImg;                   //Color map OpenCV format (BGRA)
+
+//Map to camera space point
+CameraSpacePoint* pCSPoints;
+void ShowImage(void);            //Display Image Func
+
+
+/*---------------------------------------------------------------*/
+/*----------------Region Of Interest(ROI) Declare----------------*/
+/*---------------------------------------------------------------*/
+//Initial
+Rect2i ROI_rect;
+Point ROI_p1, ROI_p2;   //start point and end point of ROI
+Mat ROI;
+bool ROI_S1 = FALSE;    //FLAG of mouse event
+bool ROI_S2 = FALSE;    //FLAG of mouse event
+void onMouseROI(int event, int x, int y, int flags, void* param);   //Mouse callback Func
+void FindROI(void);                                                 //Find ROI Func
+
+//Tracking
+int ROIcount = 0;
+Point2i ROICenterColorS_Old, ROICenterColorS_New;   //center of tracking object of ROI in Color Space
+Point2i* ROIPixel;
+CameraSpacePoint* ROICameraSP;
+void MoveROI(void);                                //Tracking ROI Func
+
+//Mapping
+int ROIDepthCount = 0;
+Point3f ROICenterCameraS;
+void CameraSpaceROI(void);                         //Mapping ROI Func
+
+
+/*------------------------------------------*/
+/*--------------OpenGL Function-------------*/
+/*------------------------------------------*/
+void GLInit()
+{
+	GLfloat  whiteLight[] = { 0.45f, 0.45f, 0.45f, 1.0f };
+	GLfloat  sourceLight[] = { 0.25f, 0.25f, 0.25f, 1.0f };
+	GLfloat	 lightPos[] = { -50.f, 25.0f, 250.0f, 0.0f };
+
+	glEnable(GL_DEPTH_TEST);
+	glFrontFace(GL_CCW);
+	glEnable(GL_CULL_FACE);
+
+	glEnable(GL_LIGHTING);
+
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, whiteLight);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, sourceLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, sourceLight);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+	glEnable(GL_LIGHT0);
+
+	glEnable(GL_COLOR_MATERIAL);
+
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void timer(int value)
+{
+	switch (value)
+	{
+		//OpenGL refresh timmer
+	case 0:
+		//printf("FPS = %d\n", FPS);
+		FPS = 0;
+		glutTimerFunc(1000, timer, 0);
+		break;
+	}
+
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'F':
+	case 'f':
+		if (IsDrawPC)
+		{
+			IsDrawPC = false;
+		}
+		else
+		{
+			IsDrawPC = true;
+		}
+		break;
+	}
+}
+
+void DrawPointCloud()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	static const double kFovY = /*54.5*/ 53.3;
+	static const double kPI = 3.1415926535897932384626433832795;
+	double nearDist, farDist, aspect;
+
+	nearDist = 0.01f / tan((kFovY / 2.0) * kPI / 180.0);
+	farDist = 20000;
+	aspect = (double)iWidthColor / iHeightColor;
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluPerspective(kFovY, aspect, nearDist, farDist);
+	//gluPerspective(54.5f, (double)iWidthColor / (double)iHeightColor, 0.01f, 20000.0f);
+
+	glEnable(GL_DEPTH_TEST);
+
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluLookAt(0, 0, 0, 0, 0, -1, 0, 1, 0);
+	glTranslatef(0.055f, -0.02f, 0.0f);
+
+	glPushMatrix();
+
+	glPointSize(1.0f);
+	glBegin(GL_POINTS);
+
+	for (int i = 0; i < colorPointCount; i++)
+	{
+		glColor3ub(pBufferColor[4 * i], pBufferColor[4 * i + 1], pBufferColor[4 * i + 2]);
+		GLfloat pX = pCSPoints[i].X;
+		GLfloat pY = pCSPoints[i].Y;
+		GLfloat pZ = -pCSPoints[i].Z;
+		glVertex3f(pX, pY, pZ);
+	}
+
+	glEnd();
+	glPopMatrix();
+
+}
+
+void SceneWithBackground()
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glViewport(0, 0, iWidthColor, iHeightColor);
+	glOrtho(0, iWidthColor, 0, iHeightColor, -1, 1);
+
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_LIGHTING);
+	glDisable(GL_BLEND);
+
+	glMatrixMode(GL_MODELVIEW);
+
+	glLoadIdentity();
+
+	//Draw backgroung through RGBA image from Kinect
+
+	glRasterPos2f(0.0f, 0.0f);
+	//glDrawPixels(iWidthColor, iHeightColor, GL_RGBA, GL_UNSIGNED_BYTE, pBufferColor);
+
+	glEnable(GL_TEXTURE_2D);
+
+	//Without painting BG white the BG will turn orange and glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) did not work. Don't know why
+	glColor3ub(255, 255, 255);
+	// tell opengl to use the generated texture name
+	glBindTexture(GL_TEXTURE_2D, textureid);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, iWidthColor, iHeightColor, 0, GL_RGBA, GL_UNSIGNED_BYTE, pBufferColor);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glPushMatrix();
+
+	// make a rectangle
+	glBegin(GL_QUADS);
+
+	// top left
+	glTexCoord2i(0, 0);
+	glVertex2i(0, iHeightColor);
+
+	// top right
+	glTexCoord2i(1, 0);
+	glVertex2i(iWidthColor, iHeightColor);
+
+	// bottom right
+	glTexCoord2i(1, 1);
+	glVertex2i(iWidthColor, 0);
+
+	// bottom left
+	glTexCoord2i(0, 1);
+	glVertex2i(0, 0);
+
+	glEnd();
+	glPopMatrix();
+
+	glDisable(GL_TEXTURE_2D);
+
+
+
+	static const double kFovY = /*54.5*/ 53.3;
+	static const double kPI = 3.1415926535897932384626433832795;
+	double nearDist, farDist, aspect;
+
+	nearDist = 0.01f / tan((kFovY / 2.0) * kPI / 180.0);
+	farDist = 20000;
+	aspect = (double)iWidthColor / iHeightColor;
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluPerspective(kFovY, aspect, nearDist, farDist);
+	//gluPerspective(54.5f, (double)iWidthColor / (double)iHeightColor, 0.01f, 20000.0f);
+
+	glEnable(GL_DEPTH_TEST);
+
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluLookAt(0, 0, 0, 0, 0, -1, 0, 1, 0);
+	glTranslatef(0.055f, -0.02f, 0.0f);
+}
+
+void RenderScene()
+{
+
+	if (IsDrawPC)
+	{
+		DrawPointCloud();
+	}
+	else
+	{
+		SceneWithBackground();
+	}
+
+	/*--------------ROI Mapping-------------*/
+	if (ROIDepthCount != 0)
+	{
+		glPushMatrix();
+		glPointSize(2.0f);
+		glBegin(GL_POINTS);
+		for (int i = 0; i < ROIDepthCount; i++)
+		{
+			glColor3ub(0, 255, 0);
+			GLfloat pX = ROICameraSP[i].X;
+			GLfloat pY = ROICameraSP[i].Y;
+			GLfloat pZ = -ROICameraSP[i].Z;
+			glVertex3f(pX, pY, pZ);
+
+		}
+		glEnd();
+		glPopMatrix();
+	}
+
+	glutSwapBuffers();
+}
+
+
+/*------------------------------------------*/
+/*--------------Kinect Function-------------*/
+/*------------------------------------------*/
+void KineticInit()
+{
+	// 1. Sensor related code
+	cout << "Try to get default sensor" << endl;
+	{
+		if (GetDefaultKinectSensor(&pSensor) != S_OK)
+		{
+			cerr << "Get Sensor failed" << endl;
+			return;
+		}
+
+		cout << "Try to open sensor" << endl;
+		if (pSensor->Open() != S_OK)
+		{
+			cerr << "Can't open sensor" << endl;
+			return;
+		}
+	}
+
+	// 2. Color related code
+	cout << "Try to get color source" << endl;
+	{
+		// Get frame source
+		IColorFrameSource* pFrameSource = nullptr;
+		if (pSensor->get_ColorFrameSource(&pFrameSource) != S_OK)
+		{
+			cerr << "Can't get color frame source" << endl;
+			return;
+		}
+
+		// Get frame description
+		cout << "get color frame description\n" << endl;
+		IFrameDescription* pFrameDescription = nullptr;
+		if (pFrameSource->get_FrameDescription(&pFrameDescription) == S_OK)
+		{
+			pFrameDescription->get_Width(&iWidthColor);
+			pFrameDescription->get_Height(&iHeightColor);
+
+			colorPointCount = iWidthColor * iHeightColor;
+			uBufferSizeColor = colorPointCount * 4 * sizeof(BYTE);
+
+			pCSPoints = new CameraSpacePoint[colorPointCount];
+			pBufferColor = new BYTE[4 * colorPointCount];
+			pBufferColor_RGB = new BYTE[4 * colorPointCount];
+		}
+		pFrameDescription->Release();
+		pFrameDescription = nullptr;
+
+		// get frame reader
+		cout << "Try to get color frame reader" << endl;
+		if (pFrameSource->OpenReader(&pFrameReaderColor) != S_OK)
+		{
+			cerr << "Can't get color frame reader" << endl;
+			return;
+		}
+
+		// release Frame source
+		cout << "Release frame source" << endl;
+		pFrameSource->Release();
+		pFrameSource = nullptr;
+	}
+
+	// 3. Depth related code
+	cout << "Try to get depth source" << endl;
+	{
+		// Get frame source
+		IDepthFrameSource* pFrameSource = nullptr;
+		if (pSensor->get_DepthFrameSource(&pFrameSource) != S_OK)
+		{
+			cerr << "Can't get depth frame source" << endl;
+			return;
+		}
+		if (pSensor->get_DepthFrameSource(&pFrameSource) == S_OK)
+		{
+			pFrameSource->get_DepthMinReliableDistance(&uDepthMin);
+			pFrameSource->get_DepthMaxReliableDistance(&uDepthMax);
+		}
+
+		// Get frame description
+		cout << "get depth frame description\n" << endl;
+		IFrameDescription* pFrameDescription = nullptr;
+		if (pFrameSource->get_FrameDescription(&pFrameDescription) == S_OK)
+		{
+			pFrameDescription->get_Width(&iWidthDepth);
+			pFrameDescription->get_Height(&iHeightDepth);
+
+			depthPointCount = iWidthDepth * iHeightDepth;
+			pBufferDepth = new UINT16[depthPointCount];
+		}
+		pFrameDescription->Release();
+		pFrameDescription = nullptr;
+
+		// get frame reader
+		cout << "Try to get depth frame reader" << endl;
+		if (pFrameSource->OpenReader(&pFrameReaderDepth) != S_OK)
+		{
+			cerr << "Can't get depth frame reader" << endl;
+			return;
+		}
+		// release Frame source
+		cout << "Release frame source" << endl;
+		pFrameSource->Release();
+		pFrameSource = nullptr;
+	}
+
+	// 4. Coordinate Mapper
+	if (pSensor->get_CoordinateMapper(&Mapper) != S_OK)
+	{
+		cerr << "get_CoordinateMapper failed" << endl;
+		return;
+	}
+
+	mDepthImg = cv::Mat::zeros(iHeightDepth, iWidthDepth, CV_16UC1);
+	mImg8bit = cv::Mat::zeros(iHeightDepth, iWidthDepth, CV_8UC1);
+	mColorImg = cv::Mat::zeros(iHeightColor, iWidthColor, CV_8UC4);
+}
+
+void KineticUpdate()
+{
+	/*--------------Read color data-------------*/
+	IColorFrame* pFrameColor = nullptr;
+	if (pFrameReaderColor->AcquireLatestFrame(&pFrameColor) == S_OK)
+	{
+		pFrameColor->CopyConvertedFrameDataToArray(uBufferSizeColor, pBufferColor, ColorImageFormat_Rgba);
+		pFrameColor->CopyConvertedFrameDataToArray(uBufferSizeColor, mColorImg.data, ColorImageFormat_Bgra);
+
+		pFrameColor->Release();
+		pFrameColor = nullptr;
+
+		{
+			FPS++;
+			glutPostRedisplay();
+		}
+
+	}
+
+	/*--------------Read depth data-------------*/
+	IDepthFrame* pDFrameDepth = nullptr;
+	if (pFrameReaderDepth->AcquireLatestFrame(&pDFrameDepth) == S_OK)
+	{
+		pDFrameDepth->CopyFrameDataToArray(depthPointCount, pBufferDepth);
+		pDFrameDepth->CopyFrameDataToArray(depthPointCount, reinterpret_cast<UINT16*>(mDepthImg.data));
+
+		pDFrameDepth->Release();
+		pDFrameDepth = nullptr;
+	}
+
+
+	/*--------------Mapper Function (Point Cloud)-------------*/
+	Mapper->MapColorFrameToCameraSpace(depthPointCount, pBufferDepth, colorPointCount, pCSPoints);
+
+	/*--------------Call Window Function-------------*/
+	ShowImage();
+}
+
+void ShowImage()
+{
+	namedWindow("Depth Map");
+	namedWindow("Color Map");
+
+	/*--------------Depth Image-------------*/
+	// Convert from 16bit to 8bit
+	mDepthImg.convertTo(mImg8bit, CV_8U, 255.0f / uDepthMax);
+
+	/*--------------Set Mouse Callback Function and Find ROI-------------*/
+	cvSetMouseCallback("Color Map", onMouseROI, NULL);
+	cv::imshow("Depth Map", mImg8bit);
+
+
+	if (ROI_S1 == false && ROI_S2 == false)
+	{
+		// Initial State
+		ROI = mColorImg.clone();
+		cv::imshow("Color Map", ROI);
+	}
+	else if (ROI_S2 == true)
+	{
+		// CV_EVENT_LBUTTONUP：ROI選取完畢
+		if (ROI_S1 == false)
+		{
+			int thickness = 2;
+
+			ROI = mColorImg.clone();
+			rectangle(ROI, ROI_p1, ROI_p2, Scalar(0, 255, 0), thickness);
+			cv::imshow("Color Map", ROI);
+
+			FindROI();
+		}
+		//CV_EVENT_MOUSEMOVE：只選了左上角
+		else
+		{
+			cv::imshow("Color Map", ROI);
+		}
+
+	}
+
+}
+
+
+
+/*---------------------------------------*/
+/*--------------ROI Function-------------*/
+/*---------------------------------------*/
+void onMouseROI(int event, int x, int y, int flags, void* param)
+{
+	int thickness = 2;
+
+	if (event == CV_EVENT_LBUTTONDOWN)
+	{
+		////当前鼠标位置（x，y）  
+		ROI_rect.x = x;
+		ROI_rect.y = y;
+
+		ROI_S1 = true;
+		ROI_S2 = false;
+
+	}
+	else if (ROI_S1&&event == CV_EVENT_MOUSEMOVE)
+	{
+		ROI_S2 = true;
+
+		ROI_p1 = Point(ROI_rect.x, ROI_rect.y);
+		ROI_p2 = Point(x, y);
+
+		ROI = mColorImg.clone();
+		rectangle(ROI, ROI_p1, ROI_p2, Scalar(0, 255, 0), 2);
+		cv::imshow("Color Map", ROI);
+
+	}
+	else if (ROI_S1&&event == CV_EVENT_LBUTTONUP)
+	{
+		ROI_S1 = false;
+
+		ROI_rect.height = y - ROI_rect.y;
+		ROI_rect.width = x - ROI_rect.x;
+
+		ROI_p2 = Point(x, y);
+
+	}
+}
+
+void FindROI()
+{
+	namedWindow("ROI");
+	namedWindow("YCrCb");
+
+	/*--------------Find ROI-------------*/
+	Mat ROI_Image = mColorImg.colRange(ROI_p1.x, ROI_p2.x + 1).rowRange(ROI_p1.y, ROI_p2.y + 1).clone();
+	imshow("ROI", ROI_Image);
+
+
+	/*--------------BGR to YCrCb-------------*/
+	Mat ROI_YCrCb;
+	cvtColor(ROI_Image, ROI_YCrCb, CV_BGR2YCrCb);
+	imshow("YCrCb", ROI_YCrCb);
+
+
+	/*-------------- Color Detection and Tracking-------------*/
+	int rows = ROI_YCrCb.rows;
+	int cols = ROI_YCrCb.cols;
+	ROIcount = 0;
+	ROIDepthCount = 0;
+
+
+	if (ROIPixel != nullptr)
+	{
+		delete[] ROIPixel;
+		ROIPixel = nullptr;
+	}
+	ROIPixel = new Point2i[1000];
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			int IsRed = (int)ROI_YCrCb.at<Vec3b>(i, j)[1];
+
+			//threshold = 150 for fluorescent pink
+			//threshold = 170 for red
+			if (IsRed > 170)
+			{
+				ROI_Image.at<Vec4b>(i, j)[0] = 255;
+
+
+				ROI_Image.at<Vec4b>(i, j)[1] = 0;
+				ROI_Image.at<Vec4b>(i, j)[2] = 0;
+
+				ROI.at<Vec4b>(i + ROI_p1.y, j + ROI_p1.x)[0] = 255;
+				ROI.at<Vec4b>(i + ROI_p1.y, j + ROI_p1.x)[1] = 0;
+				ROI.at<Vec4b>(i + ROI_p1.y, j + ROI_p1.x)[2] = 0;
+
+				ROIPixel[ROIcount].x = j + ROI_p1.x;
+				ROIPixel[ROIcount].y = i + ROI_p1.y;
+
+				ROICenterColorS_New.x += ROIPixel[ROIcount].x;
+				ROICenterColorS_New.y += ROIPixel[ROIcount].y;
+
+				ROIcount++;
+			}
+		}
+	}
+
+	imshow("ROI", ROI_Image);
+	imshow("Color Map", ROI);
+
+
+	if (ROIcount > 0)
+	{
+		ROICenterColorS_New.x = static_cast<int>(ROICenterColorS_New.x / ROIcount);
+		ROICenterColorS_New.y = static_cast<int>(ROICenterColorS_New.y / ROIcount);
+
+		CameraSpaceROI();
+		MoveROI();
+
+	}
+	else if (ROIcount == 0)
+	{
+		ROICenterColorS_Old.x = ROICenterColorS_New.x = 0;
+		ROICenterColorS_Old.y = ROICenterColorS_New.y = 0;
+	}
+
+	ROI_Image.release();
+	ROI_YCrCb.release();
+	ROI.release();
+
+}
+
+void MoveROI()
+{
+	if (ROICenterColorS_Old.x == 0 && ROICenterColorS_Old.y == 0)
+	{
+		ROICenterColorS_Old.x = ROICenterColorS_New.x;
+		ROICenterColorS_Old.y = ROICenterColorS_New.y;
+	}
+	else if (ROICenterColorS_Old.x != 0 && ROICenterColorS_New.y != 0)
+	{
+		Vec2i Dir;
+
+		Dir.val[0] = ROICenterColorS_New.x - ROICenterColorS_Old.x;
+		Dir.val[1] = ROICenterColorS_New.y - ROICenterColorS_Old.y;
+
+		ROI_p1.x = ROI_p1.x + Dir.val[0];
+		ROI_p1.y = ROI_p1.y + Dir.val[1];
+
+		ROI_p2.x = ROI_p2.x + Dir.val[0];
+		ROI_p2.y = ROI_p2.y + Dir.val[1];
+
+		ROICenterColorS_Old.x = ROICenterColorS_New.x;
+		ROICenterColorS_Old.y = ROICenterColorS_New.y;
+
+		ROICenterColorS_New.x = 0;
+		ROICenterColorS_New.y = 0;
+	}
+}
+
+void CameraSpaceROI()
+{
+	if (ROICameraSP != nullptr)
+	{
+		delete[] ROICameraSP;
+		ROICameraSP = nullptr;
+	}
+
+	ROIDepthCount = 0;
+
+	ROICenterCameraS.x = 0;
+	ROICenterCameraS.y = 0;
+	ROICenterCameraS.z = 0;
+
+	for (int i = 0; i < ROIcount; i++)
+	{
+		int index1 = ROIPixel[i].x + ROIPixel[i].y * iWidthColor;
+		if (pCSPoints[index1].Z != -1 * numeric_limits<float>::infinity())
+		{
+			ROIDepthCount++;
+
+		}
+	}
+
+	ROICameraSP = new CameraSpacePoint[ROIDepthCount];
+
+	int indx1 = 0;
+	for (int i = 0; i < ROIcount; i++)
+	{
+		int indx2 = ROIPixel[i].x + ROIPixel[i].y * iWidthColor;
+
+		if (pCSPoints[indx2].Z != -1 * numeric_limits<float>::infinity())
+			//if (_fpclass(ROICameraSP[i].Z) == 256)
+			//if (pCSPoints[index].Z <= 1 && pCSPoints[index].Z >= -1)
+		{
+			ROICameraSP[indx1].X = pCSPoints[indx2].X;
+			ROICameraSP[indx1].Y = pCSPoints[indx2].Y;
+			ROICameraSP[indx1].Z = pCSPoints[indx2].Z;
+
+			//cout << ROICameraSP[i].X << " " << ROICameraSP[i].Y << " " << ROICameraSP[i].Z << endl;
+			//cout << indx1 << " " << ROICameraSP[indx1].Z << endl;
+			//cout << _fpclass(ROICameraSP[i].Z) << endl;
+
+			ROICenterCameraS.x += ROICameraSP[indx1].X;
+			ROICenterCameraS.y += ROICameraSP[indx1].Y;
+			ROICenterCameraS.z += ROICameraSP[indx1].Z;
+
+			indx1++;
+
+		}
+	}
+
+	//if (IS_KEY_F1_UP)
+	//{
+	//	ROICameraSPStorage();
+	//}
+
+	if (ROIDepthCount > 0)
+	{
+		ROICenterCameraS.x = ROICenterCameraS.x / ROIDepthCount;
+		ROICenterCameraS.y = ROICenterCameraS.y / ROIDepthCount;
+		ROICenterCameraS.z = ROICenterCameraS.z / ROIDepthCount;
+
+	}
+	else if (ROIDepthCount == 0)
+	{
+		ROICenterCameraS.x = 0;
+		ROICenterCameraS.y = 0;
+		ROICenterCameraS.z = 0;
+	}
+
+}
+
+
+int main(int argc, char** argv)
+{
+	KineticInit();
+	GLInit();
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(iWidthColor, iHeightColor);
+	glutCreateWindow("Perspective Projection");
+	glutFullScreen();
+	glutKeyboardFunc(Keyboard);
+	glutDisplayFunc(RenderScene);
+	glutIdleFunc(KineticUpdate);
+
+	glutTimerFunc(1000, timer, 0);
+
+	glutMainLoop();
+
+
+	return 0;
+}
